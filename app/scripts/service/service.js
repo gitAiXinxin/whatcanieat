e5b28ca0b59e90ae4ae4d01a4abd66430e5afba4
zhengchun <zhengchun@souche.com>
2015-07-31 00:16:04 +0800

    api addr fixed

--- app/scripts/service/service.js
+++ app/scripts/service/service.js
@@ -4,7 +4,7 @@
 app.service('FoodService',function($http){
     return{
         getFoodInfo:function(callback){
-            $http.get("http://127.0.0.1:9300/food/").success(function(res){
+            $http.get("http://120.26.200.225:9300/food/").success(function(res){
                 if(res.code==1){
                     console.log("food service");
                     callback(res.data);
@@ -18,7 +18,7 @@ app.service('FoodService',function($http){
         // 增加食物点击数
         plusClick:function(id){
             console.log("请求",id);
-            $http.get("http://127.0.0.1:9300/food/plus/"+id).success(function(res){});
+            $http.get("http://120.26.200.225:9300/food/plus/"+id).success(function(res){});
         }
     };
 
